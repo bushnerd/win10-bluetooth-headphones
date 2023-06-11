6f8ce4b9d2623c3497d49d5cbe13be0b51a65d74
bushnerd <scutxd@gmail.com>
2023-06-11 08:55:12 +0800

    discard 110B

--- connectForce.bat
+++ connectForce.bat
@@ -1,5 +1,8 @@
+@REM Hands-Free Profile (111E)
+@REM A2DP AudioSink (110B)
+@REM 原来是110B不需要
 set /p mac=<%~dp0mac.txt
-btcom -b %mac% -r -s110b
+@REM btcom -b %mac% -r -s110b
 btcom -b %mac% -r -s111e
-btcom -b %mac% -c -s110b
+@REM btcom -b %mac% -c -s110b
 btcom -b %mac% -c -s111e
